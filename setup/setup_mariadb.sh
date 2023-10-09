1c1eb5cb5081ea59bb9fc6f5a8873f2cf12d29d7
sezfabian <cheruiyotfabian@gmail.com>
2023-10-09 11:34:04 +0300

    Database Services

--- setup/setup_mariadb.sh
+++ setup/setup_mariadb.sh
@@ -3,6 +3,6 @@ sudo apt update
 sudo apt install mariadb-server
 sudo mysql_secure_installation
 sudo mariadb -e "CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin123';"
-sudo mariadb -e GRANT ALL ON *.* TO 'admin'@'localhost' IDENTIFIED BY 'admin123' WITH GRANT OPTION;
+sudo mariadb -e "GRANT ALL ON *.* TO 'admin'@'localhost' IDENTIFIED BY 'admin123' WITH GRANT OPTION;"
 sudo mariadb -e "FLUSH PRIVILEGES;"
 sudo systemctl status mariadb
