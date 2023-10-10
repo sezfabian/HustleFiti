a3752c80362d3543cf4f9db60327dfafcdf74ae1
sezfabian <cheruiyotfabian@gmail.com>
2023-10-10 10:21:27 +0300

    mysql mvp

--- setup/hdb.sql
+++ setup/hdb.sql
@@ -1,14 +1,9 @@
 -- Seting up mysql database with admin user privileges.
-CREATE DATABASE IF NOT EXISTS hustle_db;
-GRANT ALL PRIVILEGES ON hustle_db.* TO 'admin'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'admin'@'localhost';
-FLUSH PRIVILEGES;
-
--- initialize database tables.
 USE hustle_db;
 
--- users table
+-- Initializing database tables.
 
+-- users table
 CREATE TABLE IF NOT EXISTS `users`(
     `id` VARCHAR(45) NOT NULL,
     `created_at` DATETIME NOT NULL,
@@ -21,6 +16,9 @@ CREATE TABLE IF NOT EXISTS `users`(
     `user_image_path` VARCHAR(255),
     `user_video_path` VARCHAR(255),
     `user_banner_path` VARCHAR(255),
+    `is_admin` BOOLEAN NOT NULL,
+    `is_active` BOOLEAN NOT NULL,
+    `is_verified` BOOLEAN NOT NULL,
     PRIMARY KEY (`id`)
 );
 
@@ -44,7 +42,7 @@ CREATE TABLE IF NOT EXISTS `price_packages`(
     PRIMARY KEY (`id`)
 );
 
-
+-- services table
 CREATE TABLE IF NOT EXISTS `services`(
     `id` VARCHAR(45) NOT NULL,
     `created_at` DATETIME NOT NULL,
@@ -55,7 +53,78 @@ CREATE TABLE IF NOT EXISTS `services`(
     `service_category_id` VARCHAR(45),
     `image_paths` VARCHAR(255),
     `video_paths` VARCHAR(255),
+    `banner_paths` VARCHAR(255),
+    `is_verified` BOOLEAN NOT NULL,
     PRIMARY KEY (`id`),
     FOREIGN KEY (`service_category_id`) REFERENCES `service_categories`(`id`) ON DELETE SET NULL,
     FOREIGN KEY (`price_packages_id`) REFERENCES `price_packages`(`id`) ON DELETE SET NULL
 );
+
+-- contracts table
+CREATE TABLE IF NOT EXISTS `contracts` (
+    `id` VARCHAR(45) NOT NULL,
+    `created_at` DATETIME NOT NULL,
+    `updated_at` DATETIME NOT NULL,
+    `user_id` VARCHAR(45) NOT NULL,
+    `service_id` VARCHAR(45) NOT NULL,
+    `location` VARCHAR(45) NOT NULL,
+    `duration` VARCHAR(45),
+    `price_package_id` VARCHAR(45) NOT NULL,
+    `total_amount` DECIMAL(10,2),
+    `contract_start_date` DATETIME,
+    `contract_end_date` DATETIME,
+    `contract_status` VARCHAR(45) NOT NULL,
+    `paid_amount` DECIMAL(10,2) NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE CASCADE,
+    FOREIGN KEY (`service_id`) REFERENCES `services`(`id`) ON DELETE CASCADE
+)
+
+-- payments table
+CREATE TABLE IF NOT EXISTS `payments`(
+    `id` VARCHAR(45) NOT NULL,
+    `created_at` DATETIME NOT NULL,
+    `updated_at` DATETIME NOT NULL,
+    `user_id` VARCHAR(45) NOT NULL,
+    `contract_id` VARCHAR(45) NOT NULL,
+    `amount` DECIMAL(10,2) NOT NULL,
+    `payment_method` VARCHAR(45) NOT NULL,
+    `transaction_id` VARCHAR(45) NOT NULL,
+    `phone_number` VARCHAR(45),
+    `email` VARCHAR(45),
+    `account_number` VARCHAR(45),
+    `bank` VARCHAR(45),
+    `payment_status` VARCHAR(45) NOT NULL,
+    
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE CASCADE,
+    FOREIGN KEY (`contract_id`) REFERENCES `contracts`(`id`) ON DELETE CASCADE
+)
+
+CREATE TABLE IF NOT EXISTS `service_reviews` (
+    `id` VARCHAR(45) NOT NULL,
+    `created_at` DATETIME NOT NULL,
+    `updated_at` DATETIME NOT NULL,
+    `user_id` VARCHAR(45) NOT NULL,
+    `contract_id` VARCHAR(45) NOT NULL,
+    `service_id` VARCHAR(45) NOT NULL,
+    `rating` DECIMAL(10,2) NOT NULL,
+    `comment` VARCHAR(255),
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE CASCADE,
+    FOREIGN KEY (`contract_id`) REFERENCES `contracts`(`id`) ON DELETE CASCADE,
+    FOREIGN KEY (`service_id`) REFERENCES `services`(`id`) ON DELETE CASCADE
+)
+
+CREATE TABLE IF NOT EXISTS `client_reviews` (
+    `id` VARCHAR(45) NOT NULL,
+    `created_at` DATETIME NOT NULL,
+    `updated_at` DATETIME NOT NULL,
+    `contract_id` VARCHAR(45) NOT NULL,
+    `user_id` VARCHAR(45) NOT NULL,
+    `rating` DECIMAL(10,2) NOT NULL,
+    `comment` VARCHAR(255),
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE CASCADE,
+    FOREIGN KEY (`contract_id`) REFERENCES `contracts`(`id`) ON DELETE CASCADE
+)
--- setup/setup_mysql.sh
+++ setup/setup_mysql.sh
@@ -3,3 +3,10 @@ wget https://dev.mysql.com/get/mysql-apt-config_0.8.12-1_all.deb
 sudo dpkg -i mysql-apt-config_0.8.12-1_all.deb
 sudo apt update
 sudo systemctl start mysql.service
+sudo mysql -e "CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin123';"
+sudo mysql -e "DROP DATABASE IF EXISTS hustle_db;"
+sudo mysql -e "CREATE DATABASE IF NOT EXISTS hustle_db;"
+sudo mysql -e "GRANT ALL ON hustle_db.* TO 'admin'@'localhost';"
+sudo mysql -e "FLUSH PRIVILEGES;"
+sudo mysql < setup/hdb.sql
+sudo systemctl restart mysql.service
