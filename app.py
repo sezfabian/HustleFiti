7a251a9ca247e3e5713621dfe082eea884642714
Fabian Cheruiyot <cheruiyotfabian@gmail.com>
2023-10-16 23:28:58 +0300

    Update app.py
    storage is activated in models

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 from flask_cors import CORS
 import os
 from os import getenv
-from models.engine.db_storage import DBStorage
+from models import storage
 from models.engine.mail import Email
 storage = DBStorage()
 mail = Email()
