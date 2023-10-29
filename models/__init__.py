ba958cda0ce84ff0686e116ab378afe48c1962e0
sezfabian <cheruiyotfabian@gmail.com>
2023-10-29 22:48:25 +0300

    readded storage to models init

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,13 @@
+#!/usr/bin/python3
+"""
+initialize the models package
+"""
 
+from os import getenv
+from models.engine.db_storage import DBStorage
+from models.engine.mail import Email
+
+
+storage = DBStorage()
+mail = Email()
+storage.reload()
